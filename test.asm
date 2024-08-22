4307b485a4abc1b863aea555e398f7a8b8e341c2
0xD10 <haleplisd@protonmail.com>
2024-08-23 01:50:51 +0300

    Fixed input

--- test.asm
+++ test.asm
@@ -1,3 +1,5 @@
+section .bss
+  buffer: resb 1024;
 section .text
   global main;
   extern printf;
@@ -7,7 +9,8 @@ main:
   mov rbp, rsp;
   sub rsp, 32;
   
-  call read_filename;
+  call read_input_filename;
+  call sanitize_input_filename;
   call open_filename;
   call generate_randnumb;
 
@@ -22,7 +25,7 @@ main:
 
 generate_randnumb:
   xor rbx, rbx;
-  loop1:
+  loop_start:
     rdtsc;
     mul rdx;
     mov rdx, rbp;
@@ -30,21 +33,55 @@ generate_randnumb:
     sub rdx, rbx;
     mov [rdx], rax;
     cmp rbx, 32;
-  jl loop1;
-  
+  jl loop_start;
   ret; 
 
 open_filename:
   mov rax, 2;
-  lea rdi, [rbp-32];
+  mov rdi, rbp;
   mov rsi, 0;
-  mov rdx, 1;
-  ret;
+  mov rdx, 0;
+  syscall;
+  test rax, rax;
+  js exit_NOT_SUCCESS;
 
-read_filename:
+  mov rdi, rax;
   mov rax, 0;
-  mov rdi, 0;
-  lea rsi, [rbp-32];
-  mov rdx, 31;
+  mov rsi, buffer;
+  mov rdx, 1024;
+  syscall;
+
+  test rax, rax;
+  js exit_NOT_SUCCESS;
+
+  mov rdi, 1;
+  mov rax, 1;
+  mov rsi, buffer;
   syscall;
   ret;
+
+sanitize_input_filename:
+  xor rbx, rbx;
+  loop:
+    mov rdx, rbp;
+    add rdx, rbx;
+    add rbx, 1;
+    mov al, byte [rdx];
+    cmp al, 0x0a;
+  jne loop;
+  dec rbx;
+  mov byte [rbp+rbx], 0x00;
+  ret;
+
+read_input_filename:
+  mov rax, 0;
+  mov rdi, 0;
+  mov rsi, rbp;
+  mov rdx, 32;
+  syscall;
+  ret;
+
+exit_NOT_SUCCESS:
+  mov rax, 60;   
+  mov rdi, 1;   
+  syscall;

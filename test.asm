e195a60b25f6b7088a9a3fa4dc90a4e526377a72
0xD10 <haleplisd@protonmail.com>
2025-06-12 16:48:25 +0300

    done?

--- test.asm
+++ test.asm
@@ -1,3 +1,6 @@
+section .data
+  filename db "newfile.txt", 0;
+
 section .text
   global main;
   extern printf;
@@ -11,28 +14,66 @@ main:
   call open_user_file_input;
   call generate_pseudorandom_numbers;
   call encrypt_file;
+  call write_new_output_file;
 
   mov rsp, rbp;                    
   pop rbp;
   call exit_success;
 
+write_new_output_file:
+  mov rax, 2;
+  lea rdi, [filename];
+  mov rsi, 64 | 2;
+  mov rdx, 644;
+  syscall;
+
+  mov dword [rbp-984], eax;
+
+  mov rax, 1;
+  mov rsi, [rbp-512];
+  mov rdx, [rbp-520];
+  mov rdi, [rbp-984];
+  syscall;
+
+  mov rax, 3;
+  mov edi, dword [rbp-984];
+  syscall;
+
+  mov rax, 3;
+  mov edi, dword [rbp-992];
+  syscall;
+
 encrypt_file:
   xor rax, rax;
   mov edi, dword [rbp-992];
   lea rsi, [rbp-512];
-  mov rdx, 256;
+  mov rdx, 512;
   syscall;
 
   mov qword [rbp-520], rax;
   mov byte [rbp-512+rax], 0; 
 
-  xor rbx, rbx;
+  xor r8, r8;
+  xor r9, r9;
   loop_1:
-    mov rax, qword [rbp-1024+rbx]
+    mov al, byte [rbp-1024+r8];
+    mov bl, byte [rbp-512+r9];
+    cmp bl, 0;
+    je return_main;
 
+    xor al, bl;
+    mov byte [rbp-512+r9], al;
+    inc r8;
+    inc r9;
+    cmp r8, 32;
+    je make_zero;
+  
   jne loop_1;
+  return_main:
+    ret;
+  make_zero:
+    xor r8,r8;
 
-  ret;
 
 generate_pseudorandom_numbers:
   xor rbx, rbx;
@@ -82,7 +123,7 @@ error_exit:
 ;; Variables location in stack
 ;; [rbp-1056] + 31bytes for filename and 1byte for null-byte
 ;; [rbp-1024] + 32bytes pseudo-random numbers generated with rdtsc 8bytes each
-;; [rbp-992] file_discriptor var
+;; [rbp-992] input_file_discriptor
+;; [rbp-984] output_file_discriptor
 ;; [rbp-520] filesize in bytes
-;; [rbp-512] file data MAXSIZE=256bytes
-;; [rbp-256] encrypted file data output MAXSIZE=256bytes 
+;; [rbp-512] file data MAXSIZE=512bytes

a326c95fa72d614cd09ec62e7f3f6e2de60cbc46
0xD10 <haleplisd@protonmail.com>
2025-06-12 12:40:56 +0300

    fix asm file

--- test.asm
+++ test.asm
@@ -1,5 +1,3 @@
-section .bss
-  buffer: resb 1024;
 section .text
   global main;
   extern printf;
@@ -7,23 +5,31 @@ section .text
 main:
   push rbp;
   mov rbp, rsp;
-  sub rsp, 32;
+  sub rsp, 1056;
   
-  call read_input_filename;
-  call sanitize_input_filename;
-  call open_filename;
-  call generate_randnumb;
+  call read_input_file;
+  call open_file;
+  call generate_random_numbers;
+  call encrypt_file;
 
-  ;; DEALLOCATE
   mov rsp, rbp;                    
   pop rbp;
-  ;; EXIT_SUCCESS
-  mov rax, 60;   
-  xor rdi, rdi;   
+  call exit_success;
+
+encrypt_file:
+  xor rax, rax;
+  mov rsi, rbp;
+  mov rdx, 1024;
   syscall;
+  mov byte [rbp+rax], 0;
 
+  mov rax, 1;
+  mov rdi, 1;
+  mov rsi, rbp;
+  syscall;
+  ret;
 
-generate_randnumb:
+generate_random_numbers:
   xor rbx, rbx;
   loop_start:
     rdtsc;
@@ -36,52 +42,33 @@ generate_randnumb:
   jl loop_start;
   ret; 
 
-open_filename:
+open_file:
   mov rax, 2;
-  mov rdi, rbp;
+  lea rdi, [rbp-1056];
   mov rsi, 0;
   mov rdx, 0;
   syscall;
   test rax, rax;
-  js exit_NOT_SUCCESS;
-
+  js error_exit;
   mov rdi, rax;
-  mov rax, 0;
-  mov rsi, buffer;
-  mov rdx, 1024;
-  syscall;
-
-  test rax, rax;
-  js exit_NOT_SUCCESS;
-
-  mov rdi, 1;
-  mov rax, 1;
-  mov rsi, buffer;
-  syscall;
   ret;
 
-sanitize_input_filename:
-  xor rbx, rbx;
-  loop:
-    mov rdx, rbp;
-    add rdx, rbx;
-    add rbx, 1;
-    mov al, byte [rdx];
-    cmp al, 0x0a;
-  jne loop;
-  dec rbx;
-  mov byte [rbp+rbx], 0x00;
-  ret;
-
-read_input_filename:
+read_input_file:
   mov rax, 0;
   mov rdi, 0;
-  mov rsi, rbp;
+  lea rsi, [rbp-1056];
   mov rdx, 32;
   syscall;
+  dec rax;
+  mov byte [rbp-1056+rax], 0;
   ret;
 
-exit_NOT_SUCCESS:
+exit_success:
+  mov rax, 60;
+  xor rdi, rdi;
+  syscall;
+
+error_exit:
   mov rax, 60;   
   mov rdi, 1;   
   syscall;
